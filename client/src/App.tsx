85ed16869858407b69edd873223babb22d538d57
joaqLe <146143911+joaqLe@users.noreply.github.com>
2025-06-09 00:07:39 -0400

    Add leaderboard and events pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import Dashboard from './pages/Dashboard';
 import Swap from './pages/Swap';
 import Wallet from './pages/Wallet';
 import Alerts from './pages/Alerts';
+import Leaderboard from './pages/Leaderboard';
+import Events from './pages/Events';
 import './App.css';
 
 function App() {
@@ -12,13 +14,17 @@ function App() {
         <Link to="/">Dashboard</Link> |{' '}
         <Link to="/wallet">Wallet</Link> |{' '}
         <Link to="/swap">Swap</Link> |{' '}
-        <Link to="/alerts">Alerts</Link>
+        <Link to="/alerts">Alerts</Link> |{' '}
+        <Link to="/leaderboard">Leaderboard</Link> |{' '}
+        <Link to="/events">Events</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/swap" element={<Swap />} />
         <Route path="/alerts" element={<Alerts />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/events" element={<Events />} />
       </Routes>
     </Router>
   );
